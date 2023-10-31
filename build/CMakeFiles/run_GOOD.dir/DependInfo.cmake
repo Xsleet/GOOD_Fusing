5ebf944f5910b2bfce724974d170a960e3ba6471
xWillis <xWillis@XIANYING>
2023-10-31 19:05:02 +0800

    使用CMake编译

---
+++ build/CMakeFiles/run_GOOD.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/xWillis/Desktop/GOOD_Fusing/GOOD_src/FtpUtil.cpp" "CMakeFiles/run_GOOD.dir/GOOD_src/FtpUtil.cpp.obj" "gcc" "CMakeFiles/run_GOOD.dir/GOOD_src/FtpUtil.cpp.obj.d"
+  "C:/Users/xWillis/Desktop/GOOD_Fusing/GOOD_src/PreProcess.cpp" "CMakeFiles/run_GOOD.dir/GOOD_src/PreProcess.cpp.obj" "gcc" "CMakeFiles/run_GOOD.dir/GOOD_src/PreProcess.cpp.obj.d"
+  "C:/Users/xWillis/Desktop/GOOD_Fusing/GOOD_src/StringUtil.cpp" "CMakeFiles/run_GOOD.dir/GOOD_src/StringUtil.cpp.obj" "gcc" "CMakeFiles/run_GOOD.dir/GOOD_src/StringUtil.cpp.obj.d"
+  "C:/Users/xWillis/Desktop/GOOD_Fusing/GOOD_src/TimeUtil.cpp" "CMakeFiles/run_GOOD.dir/GOOD_src/TimeUtil.cpp.obj" "gcc" "CMakeFiles/run_GOOD.dir/GOOD_src/TimeUtil.cpp.obj.d"
+  "C:/Users/xWillis/Desktop/GOOD_Fusing/GOOD_src/run_GOOD.cpp" "CMakeFiles/run_GOOD.dir/GOOD_src/run_GOOD.cpp.obj" "gcc" "CMakeFiles/run_GOOD.dir/GOOD_src/run_GOOD.cpp.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/run_GOOD.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "C:/Users/xWillis/Desktop/GOOD_Fusing/bin/librun_GOOD.dll.a"
+  "C:/Users/xWillis/Desktop/GOOD_Fusing/bin/run_GOOD.exe"
+  "C:/Users/xWillis/Desktop/GOOD_Fusing/bin/run_GOOD.exe.manifest"
+  "C:/Users/xWillis/Desktop/GOOD_Fusing/bin/run_GOOD.pdb"
+  "CMakeFiles/run_GOOD.dir/GOOD_src/FtpUtil.cpp.obj"
+  "CMakeFiles/run_GOOD.dir/GOOD_src/FtpUtil.cpp.obj.d"
+  "CMakeFiles/run_GOOD.dir/GOOD_src/PreProcess.cpp.obj"
+  "CMakeFiles/run_GOOD.dir/GOOD_src/PreProcess.cpp.obj.d"
+  "CMakeFiles/run_GOOD.dir/GOOD_src/StringUtil.cpp.obj"
+  "CMakeFiles/run_GOOD.dir/GOOD_src/StringUtil.cpp.obj.d"
+  "CMakeFiles/run_GOOD.dir/GOOD_src/TimeUtil.cpp.obj"
+  "CMakeFiles/run_GOOD.dir/GOOD_src/TimeUtil.cpp.obj.d"
+  "CMakeFiles/run_GOOD.dir/GOOD_src/run_GOOD.cpp.obj"
+  "CMakeFiles/run_GOOD.dir/GOOD_src/run_GOOD.cpp.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run_GOOD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
